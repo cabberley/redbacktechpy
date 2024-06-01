54475bc957a8d1b4bf625d30c8463714744b302a
Chris Abberley <26394346+cabberley@users.noreply.github.com>
2024-06-02 05:47:34 +1000

    Update redbacktech_client.py

--- src/redbacktechpy/redbacktech_client.py
+++ src/redbacktechpy/redbacktech_client.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta, timezone
 from bs4 import BeautifulSoup
 import logging
 
-from constants import (
+from .constants import (
     BaseUrl, 
     Endpoint, 
     Header,
@@ -17,14 +17,14 @@ from constants import (
     DEVICEINFOREFRESH,
 )
 
-from model import (
+from .model import (
     Inverters,
     Batterys,
     RedbackTechData,
     RedbackEntitys,
     DeviceInfo,
 )
-from exceptions import (
+from .exceptions import (
         AuthError, 
         RedbackTechClientError,
 )
